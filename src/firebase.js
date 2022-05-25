ba7c578091c58775414437a8ce900375b6924e99
Shailendra Vishwakarma <shailendrasv0110@gmail.com>
2022-05-25 12:28:54 +0530

    ready for deployment

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCF6UnXnsfXeBYm6b94qRgsIcqjODp82dM",
+    authDomain: "clone-e35f8.firebaseapp.com",
+    projectId: "clone-e35f8",
+    storageBucket: "clone-e35f8.appspot.com",
+    messagingSenderId: "422987227783",
+    appId: "1:422987227783:web:8c0d97632ef2505c054e22",
+    measurementId: "G-0Q4898KE5J"
+  };
+
+  export default firebase.initializeApp(firebaseConfig)
